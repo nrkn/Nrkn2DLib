0e97a75f75d7e460155cf699774ce50bf61a3a75
nrkn <nrkn.com@gmail.com>
2011-02-24 20:53:30 +1300

    minor changes to demo

--- ConsoleDemo/Program.cs
+++ ConsoleDemo/Program.cs
@@ -15,24 +15,25 @@ namespace ConsoleDemo {
         var line1 = new Line( new Point( 1, 1 ), new Point( 77, 22 ) );
         var line2 = new Line( new Point( 1, 22 ), new Point( 77, 1 ) );
 
+        //guarantee some walkable paths
         var lines = new[] { line1, line2 };
         foreach( var point in lines.SelectMany( line => line.DrunkenWalk( 0.8, noisyGrid.Bounds ) ) ) {
           noisyGrid[ point ] = 1;
         }
 
         Console.WriteLine( 
-          noisyGrid.ToString( 
-            c => 
-              RandomHelper.Random.NextDouble() > c ? 
-                Math.Floor( c * 10 ).Clamp( 0, 9 ) < 5 ? 
-                  "T" 
-                  : "t" 
-              : "." 
-          ) 
+          noisyGrid.ToString( DoubleToForestItem ) 
         );
 
         Console.Write( "Q to quit or enter to regenerate >" );
       } while( ( command = Console.ReadLine() ) != "Q" && command != "q" );
+    }
+
+    static string DoubleToForestItem( double value ) {
+      return RandomHelper.Random.NextDouble() > value ?
+        value < 0.5 ?
+          "T" : "t"
+      : ".";
     }
   }
 }
